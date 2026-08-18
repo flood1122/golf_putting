185e3ef312aedaf24f7fd0879faae849a510ace6
HONGSU <flood1122@gmail.com>
2026-08-19 00:01:17 +0900

    clean project

--- opencv/build/intermediates/cxx/Debug/395si6m4/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Android_Dev\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\golf_putting\\opencv\\.cxx\\Debug\\395si6m4\\arm64-v8a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/395si6m4/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Android_Dev\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\golf_putting\\opencv\\.cxx\\Debug\\395si6m4\\x86_64" ^
-  opencv_jni_shared
